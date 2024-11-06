be54d6a99be5cb8e75f58fad6a38573818a87e1e
Keagan Suah <suahkeagan@gmail.com>
2024-11-07 02:19:50 +0800

    Add files via upload

--- scripts/home/hero.js
+++ scripts/home/hero.js
@@ -1,47 +1,60 @@
-// Get the images that is displayed on the hero section
+// Get the images displayed in the hero section
 let stars = document.getElementById("stars");
 let moon = document.getElementById("moon");
 let plane = document.getElementById("plane");
 
-// Preset all the items postions
-moon.style.top = 100 + "px";
-stars.style.top = -100 + "px";
-plane.style.top = 1000 + "px";
-plane.style.left = -200 + "px";
+// Preset all the item positions
+moon.style.top = "100px";
+plane.style.top = "1000px";
+plane.style.left = "-200px";
 
+// Throttle scroll updates using requestAnimationFrame
+let scrollValue = 0;
+let ticking = false;
 
-// this creates the parallax effect, when the user scroll, the image items will move based on the scroll value
 window.addEventListener("scroll", () => {
-  let value = window.scrollY;
-
-
-  // Only when the scroll value is within the hero section, only then the elements will move
-  if (value < 1500) {
-    moon.style.transform = "rotate(" + value * 0.1 + "deg)";
-    moon.style.top = 100 + value * 0.95 + "px";
-    stars.style.top = -100 + value * 0.8 + "px";
-    // For the plane, since it is moving out of the screen, the speed depends on the width of the screen
-    if (window.screen.width > 1000) {
-      plane.style.left = -400 + value * 0.8 + "px";
-      plane.style.top = 1000 + value * 0.3 + "px";
-    } else if (window.screen.width < 1000 && window.screen.width > 626) {
-      plane.style.left = -400 + value * 0.5 + "px";
-      plane.style.top = 1000 + value * 0.3 + "px";
-    } else {
-      plane.style.left = -300 + value * 0.35 + "px";
-      plane.style.top = 1000 + value * 0.3 + "px";
-    }
+  scrollValue = window.scrollY;
+  if (!ticking) {
+    window.requestAnimationFrame(() => {
+      updateParallax();
+      ticking = false;
+    });
+    ticking = true;
   }
 });
 
+function updateParallax() {
+  if (scrollValue < 1500) {
+    moon.style.transform = `rotate(${scrollValue * 0.1}deg)`;
+    moon.style.top = 100 + scrollValue * 0.95 + "px";
+
+    // Make the stars move along with the scroll to create a "static" illusion
+    stars.style.top = scrollValue * 0.95 + "px";
+
+    if (window.innerWidth > 1000) {
+      plane.style.left = -400 + scrollValue * 0.8 + "px";
+      plane.style.top = 1000 + scrollValue * 0.3 + "px";
+    } else if (window.innerWidth > 626) {
+      plane.style.left = -400 + scrollValue * 0.5 + "px";
+      plane.style.top = 1000 + scrollValue * 0.3 + "px";
+    } else {
+      plane.style.left = -300 + scrollValue * 0.35 + "px";
+      plane.style.top = 1000 + scrollValue * 0.3 + "px";
+    }
+  }
+}
+
 // array of strings to be typed in the hero section
+const typing = new Typed(".multiple-text-header", {
+  strings: ["Print", "Console.log", "std::cout﹤﹤"],
+  typeSpeed: 70,
+  backSpeed: 50,
+  BackDelay: 1600,
+  loop: true,
+});
+
 const typed = new Typed(".multiple-text", {
-  strings: [
-    "Web Developer",
-    "Software Engineer",
-    "AI/ML Engineer",
-    "Data Scientist",
-  ],
+  strings: ["Web Developer", "Computer Student", "Data Analyst"],
   typeSpeed: 70,
   backSpeed: 50,
   BackDelay: 1600,
